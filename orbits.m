5ab7deadf1d8d6cee0da30bdc4dcc9ba6ddce009
Michelle Bergeron <mrb113@case.edu>
2014-02-12 16:48:41 -0500

    Skeleton for orbit calculation

---
+++ orbits.m
@@ -0,0 +1,28 @@
+% Michelle Bergeron - Mathematics Capstone %
+% Spring 2014 %
+
+% TODO description
+function orbits()
+   
+    %% TODO %%
+end
+
+% Generates the set of matrices that satisfy SL2 (mod n), given n. 
+% R[x,y]_p is defined as polynomials with powers up to p 
+% with coeffiecients mod n
+%
+% Parameters: SL2modN - Set of matrices in SL2(mod n)
+%             RxyP - Set of polynomials in R[x,y]_p
+% 
+% Return: That's a great question, I don't know yet if it'll return
+% something. We shall see.
+function mapOrbits(SL2modN, RxyP)
+
+    %% TODO %%
+end
+
+% Helper function returns the resulting polynomial when applied to the matrix
+function result = multiply(matrix, poly)
+
+    %% TODO %%
+end
--- setGeneration.m
+++ setGeneration.m
@@ -1,10 +1,11 @@
-% Michelle Bergeron - Mathematics Capstone %
-% Spring 2014 %
+% Michelle Bergeron - Mathematics Capstone, Spring 2014 
+%
+% This script generates the sets of matrices and polynomials
+% needed for evaluation. %
 
-function MatrixActions()
+function setGeneration()
     % Right now, we're concerned with SL2mod2
     SL2mod2 = generateSL2modN(2);
-    disp(SL2mod2); 
 end
 
 % Generates the set of matrices that satisfy SL2 (mod n), given n. 
@@ -25,7 +26,7 @@ function SL2modN = generateSL2modN(n)
             for c = 0:(n-1)
                 for d = 0:(n-1)
                     currentMat = [a b; c d];
-                    if (det(currentMat) ~= 0) 
+                    if (mod(det(currentMat), n) ~= 0) 
                         SL2modN(:, :, i) = currentMat;
                         i = i+1; 
                     end
@@ -35,5 +36,15 @@ function SL2modN = generateSL2modN(n)
     end
 end % wheeee
 
+% Generates the set of matrices that satisfy SL2 (mod n), given n. 
+% R[x,y]_p is defined as polynomials with powers up to p 
+% with coeffiecients mod n
 %
-function product = multiply(
+% Parameters: n - Desired coefficient modulus
+%             p - Maximum power of polynomials
+% 
+% Return: The polynomials in R[x,y]_n
+function RxyP = generateRxyP(n, p)
+    %% TODO %%
+    
+end
