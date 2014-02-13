ff40ca95c30dc56e43f618d69fc542fe4a1a4208
Michelle Bergeron <mrb113@case.edu>
2014-02-13 18:40:44 -0500

    Runs orbit mapping

--- orbits.m
+++ orbits.m
@@ -1,14 +1,51 @@
 % Michelle Bergeron - Mathematics Capstone %
 % Spring 2014 %
 
-% TODO description
+% Runs the program
 function orbits()
-    syms x y;
+syms x y;
+    Rxy2 = generateRxy2(2);
+    SL2mod2 = generateSL2modN(2);    
+    SL2mod3 = generateSL2modN(3);
+        
+    Rxy1 = generateRxy1(2);
     
-    Rxy2 = generateRxy2(2)
-    %for i = 1:Rxy2(
-    multiply2(x^2, [0 1; 1 0], 2)
-    %% TODO %%
+    disp('Mapping orbits using SL2(mod 2) and R[x,y]_1:');
+    for i = 1:length(SL2mod2)
+        currentMat = SL2mod2(:, :, i);
+        disp('Using matrix:');
+        disp(currentMat);
+        mapOrbits(currentMat, Rxy1, 1, 2); 
+        fprintf('\n');
+    end
+%     
+%     disp('Mapping orbits using SL2(mod 3) and R[x,y]_1:');
+%     for i = 1:length(SL2mod3)
+%         currentMat = SL2mod3(:, :, i);
+%         disp('Using matrix:');
+%         disp(currentMat);
+%         mapOrbits(currentMat, Rxy1, 1, 3); 
+%         fprintf('\n');
+%     end
+%         
+%     disp('Mapping orbits using SL2(mod 2) and R[x,y]_2:');
+%     for i = 1:length(SL2mod2)
+%         currentMat = SL2mod2(:, :, i);
+%         disp('Using matrix:');
+%         disp(currentMat);
+%         mapOrbits(currentMat, Rxy2, 2, 2); 
+%         fprintf('\n');
+%     end
+% 
+%         
+%     disp('Mapping orbits using SL2(mod 3) and R[x,y]_2:');
+%     for i = 1:length(SL2mod3)
+%         currentMat = SL2mod3(:, :, i);
+%         disp('Using matrix:');
+%         disp(currentMat);
+%         mapOrbits(currentMat, Rxy2, 2, 3); 
+%         fprintf('\n');
+%     end
 end
 
 % Generates the set of matrices that satisfy SL2 (mod n), given n. 
@@ -17,19 +54,32 @@ end
 %
 % Parameters: SL2modN - Set of matrices in SL2(mod n)
 %             RxyP - Set of polynomials in R[x,y]_p
-% 
-% Return: That's a great question, I don't know yet if it'll return
-% something. We shall see.
-function mapOrbits(SL2modN, RxyP)
-
-    %% TODO %%
+%             p - Power of R[x,y]_p
+%             n - modulus
+function mapOrbits(SL2modN, RxyP, p, n)
+    syms x y;
+    if(p == 1)
+        for i = 1:length(RxyP)            
+            poly = x*RxyP(1,i) + y*RxyP(2,i);
+            result = multiply1(poly, SL2modN, n);
+            fprintf('%s ----> %s\n', char(poly), char(result));
+        end
+    end
+    if(p == 2)
+        for i = 1:length(RxyP)            
+            poly = x^2*RxyP(1,i) + x*y*RxyP(2,i) + y^2*RxyP(3,i);
+            result = multiply2(poly, SL2modN, n);
+            fprintf('%s ----> %s\n', char(poly), char(result));
+        end
+    end
+    
 end
 
 % Helper function returns the resulting polynomial when applied 
 % to the 2x2 matrix. This is for R[x, y]_2 for R (mod n)
 function result = multiply2(poly, matrix, n)
     if (poly == 0)
-        result = 0;
+        result = '0';
     else
         syms x y;
         result = 0; 
