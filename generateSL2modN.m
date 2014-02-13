7b4f5721094bbc23b19926f3708d266c574e79ee
Michelle Bergeron <mrb113@case.edu>
2014-02-13 18:40:15 -0500

    Corrected definition of SL2(mod n)

--- generateSL2modN.m
+++ generateSL2modN.m
@@ -2,7 +2,7 @@
 %
 % Generates the set of matrices that satisfy SL2 (mod n), given n. 
 % SL2 (mod n) is defined as matrices with entries mod n with 
-% non-zero determinants.
+% determinant = 1
 %
 % Parameters: n - Desired modulus
 % 
@@ -18,7 +18,7 @@ function SL2modN = generateSL2modN(n)
             for c = 0:(n-1)
                 for d = 0:(n-1)
                     currentMat = [a b; c d];
-                    if (mod(det(currentMat), n) ~= 0) 
+                    if (mod(det(currentMat), n) == 1) 
                         SL2modN(:, :, i) = currentMat;
                         i = i+1; 
                     end
